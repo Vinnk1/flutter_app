009d58c3de581efa44614f59a7f3c487fef82ca9
Dmytro Dryl <dryldmytro@gmail.com>
2020-04-26 20:34:45 +0300

    simple realisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,59 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+    Directionality(
+      textDirection: TextDirection.ltr,
+      child: MyApp(),
+    )
+);
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
+
+  @override
+  State<MyApp> createState() =>_MyAppState();
+
+}
+
+class _MyAppState extends State<MyApp>{
+  int _alpha, _red,_green,_blue;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _alpha = new Random().nextInt(255);
+    _red = new Random().nextInt(255);
+    _green = new Random().nextInt(255);
+    _blue = new Random().nextInt(255);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+    return  GestureDetector(
+        onTap: (){
+          setState(() {
+            _alpha = new Random().nextInt(255);
+            _red = new Random().nextInt(255);
+            _green = new Random().nextInt(255);
+            _blue = new Random().nextInt(255);
+          });
+        },
+        child: Container(
+          color: Color.fromARGB(_alpha, _red, _green, _blue),
+          child: Center(
+            child: Text(
+              "Hey there",
+              style: TextStyle(
+                fontSize: 32,
+              ),
+            ),
+          ),
         ),
-        body: Center(
-          child: Text('Hey there'),
-        ),
-      ),
-    );
+      );
   }
+
 }
+
+
